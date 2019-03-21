cea1bd1837ea0d6bae4758030e9b4fca23c41858
SeungTaeKim <40455392+RyanKor@users.noreply.github.com>
2019-03-21 21:35:13 +0900

    Add files via upload
    3/21 멋사 6기 유현재 Python Basic 수업 과제 업로드 + 과제 레포 개설

---
+++ python_exercies/assignment1.py
@@ -0,0 +1,54 @@
+#assignment1(1-1)
+
+# total = 0
+# for i in range(1000):
+#     if i % 3 == 0:
+#         total += i
+# print(total)
+# -->166833
+
+
+#assignment2(1-2)
+
+# star = '*'
+# cnt = 10
+# while cnt>0:
+#     print(star*cnt)
+#     cnt -=1
+# --> Complete!
+
+#assignment3(1-3)
+# students_scores = [20,55,67,82,45,33,90,87,100,25]
+# average = sum(students_scores) / len(students_scores)
+# print(average)
+# --> 60.4
+
+
+#assignment4(1-4)
+# for num in range(1,101):
+#     print(num)
+# --> Complete!
+
+#assignment5(1-5)
+# A = [70,60,55,75,95,90,80,75,100]
+# ave = 0
+# for i in A:
+#  ave += i
+# print(ave/len(A))
+# --> 77.77777777777777
+
+#assignemnt6(1-6)
+#original Code
+
+'''
+numbers = [1,2,3,4,5]
+result = []
+for n in numbers:
+    if n % 2 ==1:
+        result.append(n*2)
+'''
+# numbers = [1,2,3,4,5]
+# result = [num * 2 for num in numbers if num % 2 == 1]
+# print(result)
+
+# -->[2,6,10]
