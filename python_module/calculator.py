4e05b144b80d19efecb1c11096acc5e4f7abe905
SeungTaeKim <40455392+RyanKor@users.noreply.github.com>
2019-04-02 15:35:32 +0900

    Update calculator.py

--- python_module/calculator.py
+++ python_module/calculator.py
@@ -7,17 +7,22 @@ print('  2 : sub')
 print('  3 : multiply')
 print('  4 : divide')
 print('  5 : stop')
-number = int(input(' : '))
-num1 = int(input('num1 : '))
-num2 = int(input('num2 : '))
-calNum = module.fourCal(num1,num2)
-if number == 1:
-    print(calNum.add())
-elif number == 2:
-    print(calNum.subtract())
-elif number == 3:
-    print(calNum.multiply())
-elif number == 4:
-    print(calNum.division())
-else:
-    pass
+while True:
+    number = int(input(' : '))
+    if number == 5:
+        print('The Calculator is ended.')
+        break
+    if number > 5:
+        print('Enter Right Number!')
+        number = int(input(' : '))
+    num1 = int(input('num1 : '))
+    num2 = int(input('num2 : '))
+    calNum = module.fourCal(num1,num2)
+    if number == 1:
+        print(calNum.add())
+    elif number == 2:
+        print(calNum.subtract())
+    elif number == 3:
+        print(calNum.multiply())
+    elif number == 4:
+        print(calNum.division())
