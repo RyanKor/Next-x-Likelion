7f3de056da9329dc3c37415e92f67c4e70baba48
SeungTaeKim <equus3144@gmail.com>
2019-07-14 15:26:51 +0900

    responsive css

--- HTH/HTH/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\equus\desktop\next\next-x-likelion\hth\hth\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
--- HTH/main/hth/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class HthConfig(AppConfig):
-    name = 'hth'
